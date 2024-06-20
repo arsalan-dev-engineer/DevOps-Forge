6f849ba9d8c641a706e237031cbab56786766de7
Arsalan <arsalan.dev.engineer@gmail.com>
2024-06-20 09:15:23 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,18 @@
 
-# testing pipeline to detect secrets (not a real secret)
-my_pass = "1023424234SEC324"
-print("My secrets: ", my_pass)
+# Testing pipeline to detect secrets (not real secrets)
 
-# my name is arsalan arref
+# dummy API Key
+api_key = "ABCD1234EFGH5678IJKL"
+print("API Key: ", api_key)
+
+# dummy Password
+password = "P@ssw0rd1234"
+print("Password: ", password)
+
+# dummy Credit Card Number
+credit_card = "4111111111111111"
+print("Credit Card: ", credit_card)
+
+# dummy Social Security Number
+ssn = "123-45-6789"
+print("SSN: ", ssn)
