f2edc22118932adbcf29ed35d999f9f3b2ddd695
arsalan arref <arsalan.dev.engineer@gmail.com>
2024-06-22 20:15:39 +0100

    made changes to files

--- temp-file.py
+++ temp-file.py
@@ -1,9 +1,3 @@
-
-numbers = range(0, 50+1)
-even = [e for e in numbers if e%2==0]
+numbers = range(0, 50 + 1)
+even = [e for e in numbers if e % 2 == 0]
 print(even)
-
-print("Hello")
-print("Hello 2")
-
-
